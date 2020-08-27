95e953fed239607f3edd8bce45c7c9596c2a28e3
cleanrq <qbyun130@163.com>
2020-08-27 19:19:14 +0800

    import

--- cloud-consumer-order80/src/main/java/com/qby/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/qby/springcloud/controller/OrderController.java
@@ -5,9 +5,12 @@ import com.qby.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @Slf4j
 public class OrderController {
     public static final String PAYMENT_URL = "http://localhost:8001";
@@ -15,8 +18,14 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create",
                 payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/qby/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/qby/springcloud/controller/PaymentController.java
@@ -1,13 +1,14 @@
 package com.qby.springcloud.controller;
 
-import com.netflix.discovery.converters.Auto;
 import com.qby.springcloud.entities.CommonResult;
 import com.qby.springcloud.entities.Payment;
 import com.qby.springcloud.service.PaymentService;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -27,7 +28,7 @@ public class PaymentController {
         }
     }
 
-    @GetMapping("/payment/create/{id}")
+    @GetMapping("/payment/get/{id}")
     public CommonResult<Payment> create(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
 
