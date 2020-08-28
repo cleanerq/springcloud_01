c16ddcb6888b4ebb88a9c0f7b6256efecf8f1a4f
cleanrq <qbyun130@163.com>
2020-08-28 17:54:20 +0800

    import

--- cloud-consumer-order80/src/main/java/com/qby/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/qby/springcloud/OrderMain80.java
@@ -3,7 +3,9 @@ package com.qby.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
 
