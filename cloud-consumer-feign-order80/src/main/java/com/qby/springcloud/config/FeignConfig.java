5001d9c12bd49d7d718066ba46e135ad51d3a606
cleanrq <qbyun130@163.com>
2020-09-22 14:41:22 +0800

    import

---
+++ cloud-consumer-feign-order80/src/main/java/com/qby/springcloud/config/FeignConfig.java
@@ -0,0 +1,17 @@
+package com.qby.springcloud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * feign日志配置类
+ *
+ */
+@Configuration
+public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
