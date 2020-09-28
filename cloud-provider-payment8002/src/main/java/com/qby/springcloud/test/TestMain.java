65a7c1b0b3034f9e8d5078ced339837371b22038
cleanrq <qbyun130@163.com>
2020-09-28 09:50:28 +0800

    import

---
+++ cloud-provider-payment8002/src/main/java/com/qby/springcloud/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.qby.springcloud.test;
+
+import java.time.ZonedDateTime;
+
+public class TestMain {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
