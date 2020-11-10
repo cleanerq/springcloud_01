6bee3d51e0da460338e79b0c96852bb7ecb34be5
cleanrq <qbyun130@163.com>
2020-11-10 10:11:35 +0800

    import

--- cloudalibaba-sentinel-service8401/src/main/java/com/qby/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/qby/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.qby.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +41,33 @@ public class FlowLimitController {
         }
         log.info("testD 测试RT");
         return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试RT");
+        int age = 10 / 0;
+        return "------testE";
+    }
+
+    @GetMapping("/testF")
+    public String testF() {
+        log.info("testF 测试RT");
+        int age = 10 / 0;
+        return "------testE";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o" + p1 + p2 + exception.getMessage();
     }
 
 }
