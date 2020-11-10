da405fe390c5a358279489c4f7f292110570acdb
cleanrq <qbyun130@163.com>
2020-11-10 18:17:06 +0800

    import

--- cloudalibaba-sentinel-service8401/src/main/java/com/qby/springcloud/alibaba/myhandler/CustomerBlockHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/qby/springcloud/alibaba/myhandler/CustomerBlockHandler.java
@@ -5,8 +5,12 @@ import com.qby.springcloud.entities.CommonResult;
 
 public class CustomerBlockHandler {
 
-    public static CommonResult handleException(BlockException exception) {
-        return new CommonResult(2020, "自定义限流处理信息....CustomerBlockHandler");
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(2020, "自定义限流处理信息....CustomerBlockHandler-----1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(2020, "自定义限流处理信息....CustomerBlockHandler-----2");
     }
 }
 
