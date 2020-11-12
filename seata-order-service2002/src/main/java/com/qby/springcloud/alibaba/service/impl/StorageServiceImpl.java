a8d6b93e6088e2b05a15ff8a53381c4fdf1aa4c5
cleanrq <qbyun130@163.com>
2020-11-12 19:45:38 +0800

    import

---
+++ seata-order-service2002/src/main/java/com/qby/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.qby.springcloud.alibaba.service.impl;
+
+import com.qby.springcloud.alibaba.dao.StorageDao;
+import com.qby.springcloud.alibaba.service.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Service
+@Slf4j
+public class StorageServiceImpl implements StorageService {
+    @Resource
+    private StorageDao storageDao;
+
+    // 扣减库存
+    @Override
+    public void decrease(Long productId, Integer count) {
+        log.info("------->storage-service中扣减库存开始");
+        storageDao.decrease(productId, count);
+        log.info("------->storage-service中扣减库存结束");
+    }
+}
+
