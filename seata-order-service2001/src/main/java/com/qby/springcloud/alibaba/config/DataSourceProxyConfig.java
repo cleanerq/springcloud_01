e734614e08d7c661fb1308a0be13126922dccecf
cleanrq <qbyun130@163.com>
2020-11-17 14:46:53 +0800

    import

--- seata-order-service2001/src/main/java/com/qby/springcloud/alibaba/config/DataSourceProxyConfig.java
+++ seata-order-service2001/src/main/java/com/qby/springcloud/alibaba/config/DataSourceProxyConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
@@ -27,7 +28,7 @@ public class DataSourceProxyConfig {
         return new DruidDataSource();
     }
 
-
+    @Primary
     @Bean
     public DataSourceProxy dataSourceProxy(DataSource dataSource) {
         return new DataSourceProxy(dataSource);
